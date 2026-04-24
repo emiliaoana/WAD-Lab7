10f0890f8e420af27726bde5ce20e2ea50a59a64
Emilia <emiliaoana01@gmail.com>
2026-04-24 09:04:14 +0300

    Update test class visibility and add reason for disabling environment test

--- src/test/java/com/upb/lab7_wad/Lab7Test.java
+++ src/test/java/com/upb/lab7_wad/Lab7Test.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Lab7Test {
+class Lab7Test {
     @Test
     void testAddition() {
         int result = 2 + 3;
         assertEquals(5, result);
     }
 
-    @Disabled
+    @Disabled("This test is disabled until MY_SECRET_KEY is added to GitHub secrets")
     @Test
     void environment() {
         assertEquals("ABC", System.getenv("MY_SECRET_KEY"));
